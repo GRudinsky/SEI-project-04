e9d4efdcf269ac224f033f0551adad0c6634abec
grudinsky <g.rudzianskas@gmail.com>
2019-11-13 22:40:56 +0000

    search form in progress

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,6 +2,7 @@ console.log('Hello')
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import 'bulma'
 import Home from './components/Home'
 
 const App = () => (
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,22 +7,29 @@ import '@lls/react-light-calendar/dist/index.css'
 class Home extends React.Component {
   constructor() {
     super()
-    const date = new Date()
-    // const startDate = date.getTime()
     this.state = {
-      flightResults: null
-      // startDate,
-      // endDate: new Date(startDate).setDate(date.getDate() + 6)
-      
+      flightResults: null,
+      searchData: {
+      },
+      departureCalendarActive: false,
+      returnCalendarActive: false,
+      startDate: null,
+      endDate: null,
+      dateFrom: null,
+      dateTo: null
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.toggleCalendar = this.toggleCalendar.bind(this)
+    this.handleDateChange = this.handleDateChange.bind(this)
   }
   //form functions
-  handleChange(startDate, endDate) {
-    this.setState({ startDate, endDate })
+  handleChange(e) {
+    const searchData = { ...this.state.searchData, [e.target.name]: e.target.value }
+    console.log(this.state.searchData)
+    this.setState({ searchData })
   }
-
+ 
   handleSubmit(e) {
     e.prevetDefault()
     console.log(e.target.value)
@@ -30,29 +37,108 @@ class Home extends React.Component {
       .then(res => this.setState({ flightResults: res.json() }))
       .catch(err => console.log('errors', err))
   }
+  getDate(value) {
+    const time = new Date(value)
+    return `${time.getDate()}/${time.getMonth() + 1}/${time.getFullYear()}`
+  }
+  handleDateChange(startDate, endDate) {
+    const dateFrom = this.getDate(startDate)
+    const dateTo = this.getDate(endDate)
+    this.setState({ startDate, endDate, dateFrom, dateTo })
+  }
+  toggleCalendar(e) {
+    e.target.name === 'departureDate' ? this.setState({ departureCalendarActive: true, returnCalendarActive: false }) : this.setState({ departureCalendarActive: false, returnCalendarActive: true })
+  }
 
   render() {
-
-   
 
     // fetch('/api/proxyflights')
     //   .then(res => res.json())
     //   .then(res => console.log('response', res))
     //   .catch(err => console.log(err))
 
-    console.log('state', this.state)
+  
+    const { searchData } = this.state
+    const { handleChange, toggleCalendar } = this
 
+    console.log('state', this.state)
     return (
-      <form 
-        value='form'
-        onSubmit={this.handleSubmit}
-      >
-        <Calendar 
-          handleChange = {this.handleChange}
-          startDate = {this.state.startDate}
-          endDate = {this.state.endDate}/>
-        <button className="button">Submit</button>
-      </form>
+      <section className="container">
+        <form
+          value='form'
+          onSubmit={this.handleSubmit}
+        >
+          <div className="field is-horizontal">
+            <div className="field-body">
+              <div className="field">
+                {/* <label className="label">From</label> */}
+                <div className="control">
+                  <input 
+                    className="input" 
+                    type="text"
+                    name="origin"
+                    placeholder="From"
+                    value={searchData.origin}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <input 
+                    className="input" 
+                    type="text" 
+                    name="destination"
+                    placeholder="To" 
+                    value={searchData.destination}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <input 
+                    className="input" 
+                    type="text" 
+                    name="departureDate"
+                    placeholder="Departure"
+                    // value={searchData.departureDate}
+                    value={this.state.dateFrom}
+                    onClick={toggleCalendar}
+                    onChange={handleChange} />
+                </div>
+                {this.state.departureCalendarActive &&
+                  <Calendar
+                    handleChange={this.handleDateChange}
+                    startDate={this.state.startDate}
+                    endDate={this.state.endDate} />}
+              </div>
+              <div className="field">
+                <div className="control">
+                  <input 
+                    className="input" 
+                    type="text" 
+                    name="returnDate"
+                    placeholder="Return" 
+                    value={searchData.returnDate}
+                    onChange={handleChange}
+                    onClick={toggleCalendar}
+                    // disabled
+                  />
+                  {this.state.returnCalendarActive &&
+                    <Calendar
+                      handleChange={this.handleDateChange}
+                      startDate={this.state.startDate}
+                      endDate={this.state.endDate} />}
+                </div>
+              </div>
+              <div className="control">
+                <div className="button is-info">Submit</div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </section>
     )
   }
 
